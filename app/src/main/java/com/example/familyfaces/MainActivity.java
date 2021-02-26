77e4065a4b2f2cb50dc36af3ae5a144d54a99776
Pranav <pranav.nigam11@gmail.com>
2021-02-25 22:39:56 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/familyfaces/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.familyfaces;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void record(View buttonPress) {
+        EditText personNameEditText = (EditText) findViewById(R.id.personNameEditText);
+
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 60);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 1 && data != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            VideoView videoView = new VideoView(this);
+            videoView.setVideoURI(data.getData());
+            videoView.start();
+            builder.setView(videoView).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/familyfaces/Member.java
@@ -0,0 +1,24 @@
+package com.example.familyfaces;
+
+public class Member {
+    private String name;
+    private String videoUrl;
+
+    public Member() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
